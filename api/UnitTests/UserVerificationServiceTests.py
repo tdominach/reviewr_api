804ad529bbaf8c95e54c25fb86ae0748ecc84d79
Derek Ledford <Lehquack@gmail.com>
2021-10-16 18:51:22 -0400

    Update UserVerificationServiceTests.py

--- api/UnitTests/UserVerificationServiceTests.py
+++ api/UnitTests/UserVerificationServiceTests.py
@@ -3,9 +3,14 @@ from reviewr_api.api.VerificationServices import UserVerificationService
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_GivenUsernameIsLessThanFiveCharacters_WhenVerifyUsernameCalled_ThenReturnFalse(self):
+        self.assertEqual(UserVerificationService.verifyUsername("User"), False)
 
+    def test_GivenUsernameIsMoreThanFiveCharacters_WhenVerifyUsernameCalled_ThenReturnTrue(self):
+        self.assertEqual(UserVerificationService.verifyUsername("Username"), True)
+
+    def test_GivenUsernameIsLessThanFiveCharacters_WhenVerifyUsernameCalled_ThenReturnFalse(self):
+        self.assertEqual(UserVerificationService.verifyUsername("User"), False)
 
 if __name__ == '__main__':
     unittest.main()
