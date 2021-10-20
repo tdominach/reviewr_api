f07c795b7ba8e7658bf4b49c517389f4ab215bc0
Derek Ledford <Lehquack@gmail.com>
2021-10-19 23:51:17 -0400

    Update ReviewModel.py
    Made it so users are linked to a review they create. It's weird the structure of this, though...
    
    You declare a foreign key in Reviews, and do nothing in Users. Having a foreign key in Reviews to Users is effectively a "OneToMany" relationship between the two

--- api/models/ReviewModel.py
+++ api/models/ReviewModel.py
@@ -1,10 +1,10 @@
 from django.db import models
+from .UserModel import User
 
 
 class Review(models.Model):
-    user = models.OneToOneField('User', models.DO_NOTHING, primary_key=True)
+    user = models.ForeignKey(User, models.DO_NOTHING, primary_key=True)
     review_id = models.CharField(max_length=22)
-    #yelp api thing. Talk with Josh about this. Grabbing restaurant IDs?
     upvote = models.IntegerField()
     downvote = models.IntegerField()
     vote_date = models.DateTimeField()
